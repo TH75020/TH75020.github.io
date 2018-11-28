03041dee89214273c85a2558ea56dca4c158240c
Tellec HERLUC <tellec.herluc@coding-academy.fr>
2018-11-28 09:58:04 +0100

    fichier transferé

---
+++ Epi J9 Materialize/index.js
@@ -0,0 +1,14 @@
+ src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"
+      document.addEventListener('DOMContentLoaded', function() {
+      var prenom=prompt("entrez votre prénom :");
+      var nom=prompt("entrez votre nom :" );
+  alert("bonjour +prenom + nom\n vous entrez sur le site des licornes");
+});
+      document.addEventListener('DOMContentLoaded', function() {
+        var elems = document.querySelectorAll('.sidenav');
+        var instances = M.Sidenav.init(elems);
+      });
+      document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.dropdown-trigger');
+    var instances = M.Dropdown.init(elems);
+  });
