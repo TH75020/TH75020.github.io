33d207a9d551f552a7a114f5db01907de2d0bd43
Tellec HERLUC <tellec.herluc@coding-academy.fr>
2019-01-21 22:34:34 +0100

    envoi du nouveau potfolio

---
+++ batman/index2.js
@@ -0,0 +1,20 @@
+src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"
+ document.addEventListener('DOMContentLoaded', function() {
+        var prenom =prompt("entrez votre prénom :");
+        var nom =prompt("entrez votre nom :" );
+        alert("bonjour " + prenom + " " + nom +" " + "\n vous entrez sur le site de Batman market");
+       });
+
+      document.addEventListener('DOMContentLoaded', function() {
+        var elems = document.querySelectorAll('.sidenav');
+        var instances = M.Sidenav.init(elems);
+      });
+      document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.dropdown-trigger');
+    var instances = M.Dropdown.init(elems);
+  });
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.materialboxed');
+    var instances = M.Materialbox.init(elems);
+  });
+ 
