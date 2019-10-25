fee329be3152582e1971259ba70d364393a11207
herluc <tellecherluc@yahoo.fr>
2019-10-25 21:36:32 +0200

    update patabois

---
+++ Patabois/js/main.js
@@ -0,0 +1,10 @@
+
+function showtarif() {
+  const x = document.getElementById("tarif");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none"
+  }
+
+}
